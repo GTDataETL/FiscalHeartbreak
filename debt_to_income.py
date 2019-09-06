41b403821b7d77816570e19c12dda378eb7cf502
joja92 <46788431+joja92@users.noreply.github.com>
2019-09-05 20:01:14 -0400

    third commit

--- debt_to_income.py
+++ debt_to_income.py
@@ -1,112 +1,114 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+    # In[1]:
 
 
 import pandas as pd
 
+def debt_to_income_init():
 
-# In[2]:
-
-
-debt_file = "household-debt-by-county.csv"
-debt_df = pd.read_csv(debt_file)
-debt_df.head()
+    # In[2]:
 
 
-# In[3]:
+    debt_file = "household-debt-by-county.csv"
+    debt_df = pd.read_csv(debt_file)
+    debt_df.head()
 
 
-debt_cols = ["year", "qtr", "area_fips", "low", "high"]
-debt_transformed = debt_df[debt_cols].copy()
+    # In[3]:
 
 
-# In[4]:
+    debt_cols = ["year", "qtr", "area_fips", "low", "high"]
+    debt_transformed = debt_df[debt_cols].copy()
 
 
-debt_transformed = debt_transformed.rename(columns={"low": "DtoI_low", "high": "DtoI_high"})
-debt_transformed["area_fips"]=debt_transformed["area_fips"].astype(str)
+    # In[4]:
 
 
-# In[5]:
+    debt_transformed = debt_transformed.rename(columns={"low": "DtoI_low", "high": "DtoI_high"})
+    debt_transformed["area_fips"]=debt_transformed["area_fips"].astype(str)
 
 
-county_file = "area_titles.csv"
-county_df = pd.read_csv(county_file)
-county_df.head()
+    # In[5]:
 
 
-# In[6]:
+    county_file = "area_titles.csv"
+    county_df = pd.read_csv(county_file)
+    county_df.head()
 
 
-county_cols = ["area_fips", "area_title"]
-county_transformed = county_df[county_cols].copy()
+    # In[6]:
 
 
-# In[7]:
+    county_cols = ["area_fips", "area_title"]
+    county_transformed = county_df[county_cols].copy()
 
 
-county_transformed = county_transformed.rename(columns={"area_title": "county_name"})
+    # In[7]:
 
 
-# In[8]:
+    county_transformed = county_transformed.rename(columns={"area_title": "county_name"})
 
 
-merged_df = pd.merge(debt_transformed, county_transformed, on='area_fips', how='left')
-merged_df.head(100)
+    # In[8]:
 
 
-# In[9]:
+    merged_df = pd.merge(debt_transformed, county_transformed, on='area_fips', how='left')
+    merged_df.head(100)
 
 
-cropped_df = merged_df.loc[(merged_df["year"]>=2009)]
-cropped_df.head(25)
+    # In[9]:
 
 
-# In[10]:
+    cropped_df = merged_df.loc[(merged_df["year"]>=2009)]
+    cropped_df.head(25)
 
 
-cleaned_df = cropped_df.dropna(how="any")
-cleaned_df.head(25)
+    # In[10]:
 
 
-# In[11]:
+    cleaned_df = cropped_df.dropna(how="any")
+    cleaned_df.head(25)
 
 
-split_df = cleaned_df["county_name"].str.split(pat=", ", n=1, expand=True)
-split_df.head()
+    # In[11]:
 
 
-# In[12]:
+    split_df = cleaned_df["county_name"].str.split(pat=", ", n=1, expand=True)
+    split_df.head()
 
 
-cleaned_cols = ["year", "qtr", "area_fips", "DtoI_low", "DtoI_high"]
-cleaned_transformed = cleaned_df[cleaned_cols].copy()
-
-cleaned_merged = pd.merge(cleaned_transformed, split_df, left_index=True, right_index=True, how='left')
-cleaned_merged.head()
+    # In[12]:
 
 
-# In[13]:
+    cleaned_cols = ["year", "qtr", "area_fips", "DtoI_low", "DtoI_high"]
+    cleaned_transformed = cleaned_df[cleaned_cols].copy()
+
+    cleaned_merged = pd.merge(cleaned_transformed, split_df, left_index=True, right_index=True, how='left')
+    cleaned_merged.head()
 
 
-final_df = cleaned_merged.rename(columns={0: "county_name", 1: "state"})
-final_df.head()
+    # In[13]:
 
 
-# In[18]:
+    final_df = cleaned_merged.rename(columns={0: "county_name", 1: "state"})
+    final_df.head()
 
 
-firstqtr_df = final_df.loc[(final_df["qtr"]==1)]
-firstqtr_df.head()
+    # In[18]:
 
 
-# In[19]:
+    firstqtr_df = final_df.loc[(final_df["qtr"]==1)]
+    firstqtr_df.head()
 
 
-newfinal_df = firstqtr_df.drop(columns=['qtr'])
-newfinal_df.head()
+    # In[19]:
+
+
+    newfinal_df = firstqtr_df.drop(columns=['qtr'])
+    newfinal_df.head()
+    return newfinal_df
 
 
 # In[ ]:
